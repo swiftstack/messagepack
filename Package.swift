664d4bd769aac1e7fb72cebd183533beb3695217
Tony Freeman <tonyfreeman@users.noreply.github.com>
2022-01-06 08:46:57 +0300

    [Package] Add minimum target requirements

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@ import PackageDescription
 
 let package = Package(
     name: "MessagePack",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+    ],
     products: [
         .library(
             name: "MessagePack",
